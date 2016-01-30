f17a6858b1618f11a5fb410107feb7d3e36708f6
Nisarg Shah <snisarg@gmail.com>
2016-01-29 22:50:03 -0800

    Commented code

--- Projects/1/network_backup_linear_regression.py
+++ Projects/1/network_backup_linear_regression.py
@@ -14,6 +14,7 @@ def number_from_end_string(str):
     return [float(s) for s in str.split('_') if s.isdigit()][-1]
 
 
+# Get the rows from the 'selected' index.
 def get_selected(all_elem, selected):
     result = []
     for i in selected:
@@ -23,6 +24,7 @@ def get_selected(all_elem, selected):
 network_file = numpy.genfromtxt('../../Datasets/network_backup_dataset.csv',
                                 delimiter=',', skip_header=1,
                                 converters={1: day_to_number, 3: number_from_end_string, 4: number_from_end_string})
+# Split files in X attribute columns and the result column object
 network_X = network_file[:, (0, 1, 2, 3, 4, 6)]
 network_Y = network_file[:, 5]
 
@@ -35,20 +37,21 @@ predicted = []
 for i in range(len(network_X)):
     predicted.append(0)
 
-for train_index, test_index in kf:
+for train_index, test_index in kf:  #Iterate over the KFold indexes
+    # KFold gives indexes, get rows of these indexes appropriately.
     network_X_train = get_selected(network_X, train_index)
     network_X_test = get_selected(network_X, test_index)
     network_Y_train = get_selected(network_Y, train_index)
     network_Y_test = get_selected(network_Y, test_index)
 
     regr = linear_model.LinearRegression()
-    regr.fit(network_X_train, network_Y_train)
+    regr.fit(network_X_train, network_Y_train)      # Train
 
     coefficient_matrix.append(regr.coef_)
-    predicted_values = regr.predict(network_X_test)
+    predicted_values = regr.predict(network_X_test)     # Guess
     i = 0
     for index in test_index:
-        predicted[index] = predicted_values[i]
+        predicted[index] = predicted_values[i]      # Record predicted value at the right index
         i += 1
 
     rmse.append(numpy.mean(predicted_values - network_Y_test) ** 2)
