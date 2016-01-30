90e836afc7fdc986eccd212be61472fcc81e7ad6
Nisarg Shah <snisarg@gmail.com>
2016-01-29 17:24:48 -0800

    KFold example

---
+++ Experiments/KFold.py
@@ -0,0 +1,18 @@
+import CSVtoArray
+from sklearn.cross_validation import KFold
+
+kf = KFold(18580, 10, True)
+
+values = []
+
+for i in range(18580):
+    values.insert(i, False)
+
+for train, test in kf:
+    for i in test:
+        values[i] = True
+
+for i in range(len(values)):
+    if values[i] is False:
+        print i
+
