25a0d6bdae14d0a28a6382b58818f670e1fd0dee
Nisarg Shah <snisarg@gmail.com>
2016-01-29 16:21:16 -0800

    Linear regression with randomisation

--- Projects/1/network_backup_linear_regression.py
+++ Projects/1/network_backup_linear_regression.py
@@ -13,21 +13,33 @@ def day_to_number(str):
 def number_from_end_string(str):
     return [float(s) for s in str.split('_') if s.isdigit()][-1]
 
+
+def gen_test_set(data_x, data_y, size):
+    data_x_test = []
+    data_x_train = []
+    data_y_test = []
+    data_y_train = []
+    for i in range(0, int(data_x.shape[0]/size)):
+        rand_pos = numpy.random.random_integers(0, size-1)
+        data_x_train.extend(data_x[i:i+rand_pos])
+        data_x_test.append(data_x[i+rand_pos])
+        data_x_train.extend(data_x[i+rand_pos+1:i+size])
+        data_y_train.extend(data_y[i:i+rand_pos])
+        data_y_test.append(data_y[i+rand_pos])
+        data_y_train.extend(data_y[i+rand_pos+1:i+size])
+    data_x_train.extend(data_x[-int(data_x.shape[0] % size):])
+    data_y_train.extend(data_y[-int(data_x.shape[0] % size):])
+    return data_x_train, data_x_test, data_y_train, data_y_test
+
 network_file = numpy.genfromtxt('../../Datasets/network_backup_dataset.csv',
                                 delimiter=',', skip_header=1,
                                 converters={1: day_to_number, 3: number_from_end_string, 4: number_from_end_string})
-
-#print network_file[:30]
-
 network_X = network_file[:, (0, 1, 2, 3, 4, 6)]
-
 network_Y = network_file[:, 5]
 
-network_X_train = network_X[:-3600]
-network_X_test = network_X[-3600:]
+network_X_train, network_X_test, network_Y_train, network_Y_test = gen_test_set(network_X, network_Y, 10)
 
-network_Y_train = network_Y[:-3600]
-network_Y_test = network_Y[-3600:]
+#print len(network_X_train), len(network_X_test), len(network_Y_train), len(network_Y_test)
 
 regr = linear_model.LinearRegression()
 regr.fit(network_X_train, network_Y_train)
