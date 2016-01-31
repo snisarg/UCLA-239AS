5118ecc0000b1f787112b569da2face965f6600c
nisargmodi <nisarg92modi@gmail.com>
2016-01-30 23:14:24 -0800

    Regularization, lasso

---
+++ Projects/1/boston_housing_regularization_lasso.py
@@ -0,0 +1,36 @@
+#From http://scikit-learn.org/stable/auto_examples/linear_model/plot_ols.html
+
+import numpy
+from sklearn import linear_model, cross_validation
+import matplotlib.pyplot as plt
+from bokeh.properties import Auto
+
+
+def get_selected(all_elem, selected):
+    result = []
+    for i in selected:
+        result.append(all_elem[i])
+    return result
+
+housing_file = numpy.genfromtxt('../../Datasets/housing_data.csv', delimiter=',', skip_header=1)
+housing_X = housing_file[:, (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)]
+housing_Y = housing_file[:, 13]
+
+kf = cross_validation.KFold(len(housing_X), 10, True)
+
+rmse = []
+predicted = []
+for i in range(len(housing_X)):
+    predicted.append(0)
+
+alphas = [0.1, 0.01, 0.001]
+    
+reglzn = linear_model.LassoCV(0.001, 3, alphas, True, False, 'auto', 100, 0.0001, True, kf, False, -1, False, None, 'cyclic')
+reglzn.fit(housing_X, housing_Y)
+
+predicted_values = reglzn.predict(housing_X)
+
+rmse.append(numpy.sqrt(((predicted_values - housing_Y) ** 2).mean()))
+
+print 'RMSE: \n', rmse
+print 'Alpha: \n', reglzn.alpha_
