5a68b8f601a72b946f42a377a17670a671ec3737
Ashish Nerkar <ashishnerkar1@gmail.com>
2016-02-14 15:06:27 -0800

    Updated functions and comments.

--- Projects/2/docs_histogram.py
+++ Projects/2/docs_histogram.py
@@ -2,12 +2,12 @@ from sklearn.datasets import fetch_20newsgroups
 import matplotlib.pyplot as pyplot
 import collections
 
-def category_frequency_count(category):
+def category_docs_frequency_count(category):
 
     category_train = fetch_20newsgroups(subset='train', categories=category, shuffle=True, random_state=42)
     frequency = []
-    frequency = collections.Counter(category_train.target)
-    docs_count = sum(frequency.values())
+    frequency = collections.Counter(category_train.target) #count frequency of category ids
+    docs_count = sum(frequency.values()) #sum up frequencies of docs of a category
     return docs_count
 
 
@@ -23,10 +23,10 @@ pyplot.show()
 
 # get frequency of docs in Comp Tech category
 comp_tech_categories = ['comp.graphics','comp.os.ms-windows.misc','comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware','comp.windows.x']
-comp_tech_freq = category_frequency_count(comp_tech_categories)
+comp_tech_freq = category_docs_frequency_count(comp_tech_categories)
 print comp_tech_freq
 
 # get frequency of docs in Recreational category
 rec_categories = ['rec.autos', 'rec.motorcycles', 'rec.sport.baseball', 'rec.sport.hockey']
-rec_freq = category_frequency_count(rec_categories)
+rec_freq = category_docs_frequency_count(rec_categories)
 print rec_freq
