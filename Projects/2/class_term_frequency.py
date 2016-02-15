827acd68f83c6f8256e9b579a794883e4455d17a
Ashish Nerkar <ashishnerkar1@gmail.com>
2016-02-15 01:06:48 -0800

    Que b: TF-IDF representation of docs

--- Projects/2/class_term_frequency.py
+++ Projects/2/class_term_frequency.py
@@ -10,3 +10,22 @@ tf_idf = TfidfVectorizer(stop_words='english',use_idf=True)
 #rows : documents, columns : terms, each row is a vector for a document
 tf_idf_data = tf_idf.fit_transform(docs_train.data)
 print tf_idf_data.shape     # Get no of keywords
+
+
+
+
+# returns top 'n' significant terms for the given category
+def get_significant_terms(category, n):
+
+
+
+    return
+
+
+docs_train = fetch_20newsgroups(subset='train',shuffle=True,random_state=42)
+
+categories = ['comp.sys.ibm.pc.hardware', 'comp.sys.mac.hardware','misc.forsale','soc.religion.christian']
+n = 10
+for category in categories:
+    terms = get_significant_terms(category, n)
+    print terms
