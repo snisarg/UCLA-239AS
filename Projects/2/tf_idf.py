96c388d5ab6fad94bac7056c9b321301a5891ed3
Ashish Nerkar <ashishnerkar1@gmail.com>
2016-02-17 20:06:30 -0800

    Que b: TF-IDF representation of docs

--- Projects/2/tf_idf.py
+++ Projects/2/tf_idf.py
@@ -7,6 +7,6 @@ docs_train = fetch_20newsgroups(subset='train',shuffle=True,random_state=42)
 tf_idf = TfidfVectorizer(stop_words='english',use_idf=True)
 
 #get Tf-idf-weighted document-term matrix
-#rows : documents, columns : terms, each row is a vector for a document
+#rows represent documents, columns represent terms, each row is a vector for a document
 tf_idf_data = tf_idf.fit_transform(docs_train.data)
 print tf_idf_data.shape     # Get no of keywords
