754bc174d692147732b0eb21c9080b43cfc5f6e8
nisargmodi <nisarg92modi@gmail.com>
2016-02-20 15:15:53 -0800

    learning algos helper added in utility

--- Projects/2/learning_algos.py
+++ Projects/2/learning_algos.py
@@ -1,32 +1,16 @@
-from sklearn.datasets import fetch_20newsgroups
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.decomposition import TruncatedSVD
-from sklearn.svm import SVC
 import utility
-from sklearn.pipeline import Pipeline
+from sklearn.datasets import fetch_20newsgroups
 
 # Load the dataset
 docs_train = fetch_20newsgroups(subset='train',shuffle=True,random_state=42)
 docs_test = fetch_20newsgroups(subset='test',shuffle=True,random_state=42)
 
-tf_idf = TfidfVectorizer(preprocessor=utility.clean_word, use_idf=True)
-
-#get Tf-idf-weighted document-term matrix
-#rows represent documents, columns represent terms, each row is a vector for a document
-#tf_idf_data = tf_idf.fit_transform(docs_train.data)
-
-#LSA ->
-lsa = TruncatedSVD(n_components=50, n_iter=5, random_state=25)
-#lsa_transformed = lsa.fit_transform(lsa.data) #not sure about this part
-
-pipeline = Pipeline([('tfidf', tf_idf),
-                            ('svd', lsa)])
-
-pipeline_transformed = pipeline.fit_transform(docs_train.data)
+learning_algo = ''
+pipeline_transformed = utility.pipeline_setup(learning_algo)
 print pipeline_transformed.shape
 
 #SVM ->
-svm = SVC(kernel='linear', probability=True, random_state=40)
-svm_transformed = svm.fit(docs_train.data, docs_train.target)
-predict = svm_transformed.predict(docs_test.data)
+# svm = SVC(kernel='linear', probability=True, random_state=40)
+# svm_transformed = svm.fit(docs_train.data, docs_train.target)
+# predict = svm_transformed.predict(docs_test.data)
 #not sure about confusion matrix and roc curve
--- Projects/2/utility.py
+++ Projects/2/utility.py
@@ -1,6 +1,9 @@
 import string
-import sklearn
+from sklearn import feature_extraction
 import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.pipeline import Pipeline
 
 stemmer = nltk.stem.LancasterStemmer()
 
@@ -12,7 +15,7 @@ def punctuation_cleaner(s):
 
 
 def stop_word_cleaner(s):
-    if s not in sklearn.feature_extraction.text.ENGLISH_STOP_WORDS:
+    if s not in feature_extraction.text.ENGLISH_STOP_WORDS:
         return True
     return False
 
@@ -29,4 +32,11 @@ def clean_word(s):
                 result += " " + stem_cleaner(w)
     return result
 
+
+def pipeline_setup(learning_algo):
+    tf_idf = TfidfVectorizer(preprocessor=clean_word, use_idf=True)
+    lsa = TruncatedSVD(n_components=50, n_iter=5, random_state=25)
+    pipeline_list = [('tf_idf', tf_idf), ('svd', lsa), ('learning_algo', learning_algo)]
+    pipeline = Pipeline(pipeline_list)
+    return pipeline
 
