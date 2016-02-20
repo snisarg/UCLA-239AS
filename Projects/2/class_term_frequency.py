08e522476f8cb8140e7dc0be72ee0f26819447ce
Nisarg Shah <snisarg@gmail.com>
2016-02-20 11:34:00 -0800

    Reduced vector for words

--- Projects/2/class_term_frequency.py
+++ Projects/2/class_term_frequency.py
@@ -1,18 +1,17 @@
 from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
+import utility
 
 # Load the dataset
 docs_train = fetch_20newsgroups(subset='train',shuffle=True,random_state=42)
 
-tf_idf = TfidfVectorizer(stop_words='english',use_idf=True)
+tf_idf = TfidfVectorizer(preprocessor=utility.clean_word, use_idf=True)
 
-#get Tf-idf-weighted document-term matrix
-#rows : documents, columns : terms, each row is a vector for a document
+# get Tf-idf-weighted document-term matrix
+# rows : documents, columns : terms, each row is a vector for a document
 tf_idf_data = tf_idf.fit_transform(docs_train.data)
 print tf_idf_data.shape     # Get no of keywords
-
-
-
+print docs_train.target[:20]
 
 # returns top 'n' significant terms for the given category
 def get_significant_terms(category, n):
