d8c3540e9d3d824307f75dbf3fb234ed06f9f076
nisargmodi <nisarg92modi@gmail.com>
2016-02-20 17:33:56 -0800

    variable changes in SVM, for other learning algos

--- Projects/2/learning_algos.py
+++ Projects/2/learning_algos.py
@@ -13,12 +13,16 @@ docs_test = fetch_20newsgroups(subset='test',shuffle=True,random_state=42)
 
 #SVM ->
 svm = SVC(kernel='linear', probability=True, random_state=40)
-pipeline = utility.pipeline_setup(svm)
-pipeline = pipeline.fit(docs_train.data, docs_train.target)
-svm_predict = pipeline.predict(docs_test.data)
+pipeline = utility.pipeline_setup(svm) #pipeline obj to be used in all algos
+
+pipeline_fitted = pipeline.fit(docs_train.data, docs_train.target)
+svm_predict = pipeline_fitted.predict(docs_test.data)
 #svm_pred_proba = pipeline.predict_proba(docs_test.data)
 
 print("Confusion matrix for SVM:\n%s" %metrics.confusion_matrix(docs_test.target, svm_predict))
 print("Precision-Recall metrics for SVM:\n")
 print metrics.precision_recall_fscore_support(docs_test.target, svm_predict, target_names=docs_test.target_names)
 print("Accuracy for SVM:\n%s" %metrics.accuracy_score(docs_test.target, svm_predict))
+
+
+             
