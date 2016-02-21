33d93f47cb9c0e6f1f4eeb1420afa79e0f9102a0
nisargmodi <nisarg92modi@gmail.com>
2016-02-20 17:28:25 -0800

    linear svm

--- Projects/2/learning_algos.py
+++ Projects/2/learning_algos.py
@@ -1,16 +1,24 @@
 import utility
 from sklearn.datasets import fetch_20newsgroups
+from sklearn.svm import SVC
+from sklearn import metrics
 
 # Load the dataset
-docs_train = fetch_20newsgroups(subset='train',shuffle=True,random_state=42)
+docs_train = fetch_20newsgroups(subset='train', shuffle=True,random_state=42)
 docs_test = fetch_20newsgroups(subset='test',shuffle=True,random_state=42)
 
-learning_algo = ''
-pipeline_transformed = utility.pipeline_setup(learning_algo)
-print pipeline_transformed.shape
+# learning_algo = ''
+# pipeline_transformed = utility.pipeline_setup(learning_algo)
+# print pipeline_transformed.shape
 
 #SVM ->
-# svm = SVC(kernel='linear', probability=True, random_state=40)
-# svm_transformed = svm.fit(docs_train.data, docs_train.target)
-# predict = svm_transformed.predict(docs_test.data)
-#not sure about confusion matrix and roc curve
+svm = SVC(kernel='linear', probability=True, random_state=40)
+pipeline = utility.pipeline_setup(svm)
+pipeline = pipeline.fit(docs_train.data, docs_train.target)
+svm_predict = pipeline.predict(docs_test.data)
+#svm_pred_proba = pipeline.predict_proba(docs_test.data)
+
+print("Confusion matrix for SVM:\n%s" %metrics.confusion_matrix(docs_test.target, svm_predict))
+print("Precision-Recall metrics for SVM:\n")
+print metrics.precision_recall_fscore_support(docs_test.target, svm_predict, target_names=docs_test.target_names)
+print("Accuracy for SVM:\n%s" %metrics.accuracy_score(docs_test.target, svm_predict))
