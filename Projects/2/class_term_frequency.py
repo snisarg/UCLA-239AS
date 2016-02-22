b30b323ebf838f8f3102189cc5948d9757eddf51
Ashish Nerkar <ashishnerkar1@gmail.com>
2016-02-21 22:23:09 -0800

    Que c : get significant terms from per class of documents

--- Projects/2/class_term_frequency.py
+++ Projects/2/class_term_frequency.py
@@ -22,7 +22,7 @@ def get_significant_terms(category):
     tf_icf_list = []
     # log base 2 in calculation
     for i in range(len(class_freq_list)):
-        tf_icf_list.append((0.5 + (0.5 * ( class_freq_list[i]) / class_max_freq)) * (math.log((20/class_count_list[i] ), 2)))
+        tf_icf_list.append(((0.5 + (0.5 * ( class_freq_list[i]) / class_max_freq))) * (math.log((20/class_count_list[i] ), 2)))
 
     top_ten_indexes = []
     top_ten_indexes = sorted(range(len(tf_icf_list)), key=lambda i: tf_icf_list[i])[-10:]
