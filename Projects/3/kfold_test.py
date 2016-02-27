1de1525ead1acad57567dd7ed4826eab7b768855
Nisarg Shah <snisarg@gmail.com>
2016-02-27 14:25:29 -0800

    Proof for empty row and column. Awaiting response

--- Projects/3/kfold_test.py
+++ Projects/3/kfold_test.py
@@ -8,13 +8,30 @@ K_VALUE = 100
 test_error = []
 test_index = -1
 
-kf = KFold(100000, 10, True)
+kf = KFold(100000, 100, True)
 for train, test in kf:
     test_index += 1
     local_error = 0
 
     r, w, test_rows = utility.r_skiplist(test)
     # r, w = utility.get_R()
+
+    # Test for empty rows.
+    for row in w:
+        sum = 0
+        for element in row:
+            sum += element
+        if sum == 0:
+            print sum
+
+    print "Switching to columns now"
+    for column in w.T:
+        sum = 0
+        for element in column:
+            sum += element
+        if sum == 0:
+            print sum
+
     u, v = utility.nmf(r, K_VALUE, w)
     uv = numpy.dot(u, v)
 
