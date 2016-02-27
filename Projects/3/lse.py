49a216bce3f7959871ceaf64188d2278d95618f3
Nisarg Shah <snisarg@gmail.com>
2016-02-27 09:30:47 -0800

    LSE calculation complete

---
+++ Projects/3/lse.py
@@ -0,0 +1,17 @@
+import utility
+import numpy
+
+r, w = utility.get_R()
+
+for k in [10, 50, 100]:
+    U, V = utility.nmf(r, k)
+    uv = numpy.dot(U, V)
+    lse = 0
+
+    for i in range(len(r)):
+        for j in range(len(r[i])):
+            if w[i,j] == 1:
+                lse += (r[i, j] - uv[i, j])**2
+
+    print 'For %d latent terms, LSE: %f' % (k, lse)
+
--- Projects/3/utility.py
+++ Projects/3/utility.py
@@ -1,12 +1,76 @@
 import numpy
+from numpy import dot
+import scipy.linalg
 
-
+'''
+    Shape is (943, 1682)
+'''
 def get_R():
     my_data = numpy.genfromtxt('../../Datasets/ml-100k/u.data', delimiter='\t')
     max_users = numpy.max(my_data[:, 0])
     max_movies = numpy.max(my_data[:, 1])
     r = numpy.zeros((max_users, max_movies))
+    w = numpy.zeros((max_users, max_movies))
     for row in my_data:
         r[row[0]-1, row[1]-1] = row[2]
-    return r
+        w[row[0]-1, row[1]-1] = 1
+    return r, w
 
+
+def nmf(X, latent_features, max_iter=100, error_limit=1e-6, fit_error_limit=1e-6):
+    """
+    Decompose X to A*Y
+    """
+    eps = 1e-5
+    print('Starting NMF decomposition with {} latent features and {} iterations.'.format(latent_features, max_iter))
+    # X = X.toarray()  # I am passing in a scipy.linalg sparse matrix
+
+    # mask
+    mask = numpy.sign(X)
+
+    # initial matrices. A is random [0,1] and Y is A\X.
+    rows, columns = X.shape
+    A = numpy.random.rand(rows, latent_features)
+    A = numpy.maximum(A, eps)
+
+    Y = scipy.linalg.lstsq(A, X)[0]
+    bool_mask = mask.astype(bool)
+    for i in range(columns):
+        Y[:,i] = scipy.linalg.lstsq(A[bool_mask[:,i],:], X[bool_mask[:,i],i])[0]
+    Y = numpy.maximum(Y, eps)
+
+    masked_X = mask * X
+    X_est_prev = dot(A, Y)
+    for i in range(1, max_iter + 1):
+        # ===== updates =====
+        # Matlab: A=A.*(((W.*X)*Y')./((W.*(A*Y))*Y'));
+        top = dot(masked_X, Y.T)
+        bottom = (dot((mask * dot(A, Y)), Y.T)) + eps
+        A *= top / bottom
+
+        A = numpy.maximum(A, eps)
+        # print 'A',  numpy.round(A, 2)
+
+        # Matlab: Y=Y.*((A'*(W.*X))./(A'*(W.*(A*Y))));
+        top = dot(A.T, masked_X)
+        bottom = dot(A.T, mask * dot(A, Y)) + eps
+        Y *= top / bottom
+        Y = numpy.maximum(Y, eps)
+        # print 'Y', numpy.round(Y, 2)
+
+
+        # ==== evaluation ====
+        if i == max_iter:
+            # print('Iteration {}:'.format(i))
+            X_est = dot(A, Y)
+            err = mask * (X_est_prev - X_est)
+            fit_residual = numpy.sqrt(numpy.sum(err ** 2))
+            X_est_prev = X_est
+
+            curRes = scipy.linalg.norm(mask * (X - X_est), ord='fro')
+            print('fit residual', numpy.round(fit_residual, 4))
+            print('total residual', numpy.round(curRes, 4))
+            if curRes < error_limit or fit_residual < fit_error_limit:
+                break
+
+    return A, Y
