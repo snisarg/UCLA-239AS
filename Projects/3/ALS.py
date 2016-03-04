841bcb433ac494828acc116cb9ecfce909440f09
Ashish Nerkar <ashishnerkar1@gmail.com>
2016-03-04 00:27:33 -0800

    Que 4: Alternating Least Squares with lambda, ROC

--- Projects/3/ALS.py
+++ Projects/3/ALS.py
@@ -1,89 +1,69 @@
 import utility
-import numpy
+import numpy as np
 
 
 # Que 4 part A
-
-r, w = utility.get_R()
-
-print r
-
-
-# Que 4 part B
 
 
 r, w = utility.get_R()
 
 for k in [10, 50, 100]:
-    U, V = utility.nmf(r, k, w)
-    print U.shape
-    print V.shape
-    uv = numpy.dot(U, V)
-    lse = 0
+    U, V = utility.nmf(X=w, latent_features=k, mask=r)
+    uv = np.dot(U, V)
+    lse = np.sum((r * (w - uv)**2))
+    #lse = 0
+    #lse = (r - uv) ** 2
+    #lse = sum(sum(np.multiply(w,lse)))
 
-    for lambda_val in [0.01, 0.1, 1]:
-        lse = 0
-        for i in range(len(r)):
-            for j in range(len(r[i])):
-                    #if w[i, j] == 1:
-                    #lse += (((r[i, j] - uv[i, j])**2) + (lambda_val * (U[i, j]**2 + V[i, j]**2)))
-                if i < 943 and j < 10:
-                    u_term = U[i, j]**2
-                else:
-                    u_term = 0
-
-                if i < 10 and j < 1682:
-                    v_term = V[i, j]**2
-                else:
-                    v_term = 0
-
-                lse += (w[i, j] * ((r[i, j] - uv[i, j])**2)) + (lambda_val * (u_term + v_term))
-        print "for lambda"
-        print lambda_val
-        print 'For %d latent terms, LSE: %f' % (k, lse)
-
+    print 'For %d latent terms, LSE: %f' % (k, lse)
 
 '''
+correct output Que 4 part A
+
 Starting NMF decomposition with 10 latent features and 100 iterations.
-('fit residual', 915.80409999999995)
-('total residual', 245.42580000000001)
-(943L, 10L)
-(10L, 1682L)
-for lambda
-0.01
-For 10 latent terms, LSE: 60695.495347
-for lambda
-0.1
-For 10 latent terms, LSE: 64850.338755
-for lambda
-1
-For 10 latent terms, LSE: 106398.772836
+('fit residual', 935.46429999999998)
+('total residual', 17.4328)
+For 10 latent terms, LSE: 82.043269
 Starting NMF decomposition with 50 latent features and 100 iterations.
-('fit residual', 605.60199999999998)
-('total residual', 174.58510000000001)
-(943L, 50L)
-(50L, 1682L)
-for lambda
-0.01
-For 50 latent terms, LSE: 30661.868826
-for lambda
-0.1
-For 50 latent terms, LSE: 32299.068705
-for lambda
-1
-For 50 latent terms, LSE: 48671.067495
+('fit residual', 571.5018)
+('total residual', 26.6343)
+For 50 latent terms, LSE: 192.148042
 Starting NMF decomposition with 100 latent features and 100 iterations.
-('fit residual', 476.86939999999998)
-('total residual', 132.8989)
-(943L, 100L)
-(100L, 1682L)
-for lambda
-0.01
-For 100 latent terms, LSE: 17754.303971
-for lambda
-0.1
-For 100 latent terms, LSE: 18583.902009
-for lambda
-1
-For 100 latent terms, LSE: 26879.882383
+('fit residual', 378.60300000000001)
+('total residual', 23.007999999999999)
+For 100 latent terms, LSE: 144.922050
 '''
+
+
+# Que 4 part B
+
+def weightedRegALS(Q, lambda_, n_factors, W, n_iterations):
+    m, n = Q.shape
+
+    X = 5 * np.random.rand(m, n_factors)
+    Y = np.linalg.lstsq(X, Q)[0]
+
+    weighted_errors = []
+    totalError =0
+    for ii in range(n_iterations):
+        for u, Wu in enumerate(W):
+            X[u] = np.linalg.solve(np.dot(Y, np.dot(np.diag(Wu), Y.T)) + lambda_ * np.eye(n_factors),
+                                   np.dot(Y, np.dot(np.diag(Wu), Q[u].T))).T
+        for i, Wi in enumerate(W.T):
+            Y[:,i] = np.linalg.solve(np.dot(X.T, np.dot(np.diag(Wi), X)) + lambda_ * np.eye(n_factors),
+                                     np.dot(X.T, np.dot(np.diag(Wi), Q[:, i])))
+        # weighted_errors.append(get_error(Q, X, Y, W))
+        # totalError += get_error(Q, X, Y, W)
+        if(ii == n_iterations - 1):
+            print('Total Error {}'.format(totalError))
+    weighted_Q_hat = np.dot(X,Y)
+    return weighted_Q_hat
+
+lambda_values = [0.01, 0.1, 1]
+r,w = utility.get_R()
+k = 10
+
+for lambda_val in lambda_values:
+    weightedRegALS(r,lambda_val, k, w, 20)
+
+
