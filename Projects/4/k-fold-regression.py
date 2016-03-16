06768e3952066aea2cc67a1a207f2d9cf0ac9dd4
Ashish Nerkar <ashishnerkar1@gmail.com>
2016-03-15 23:15:27 -0700

    Code for K-fold cross validation with Linear Regression for que 4

--- Projects/4/k-fold-regression.py
+++ Projects/4/k-fold-regression.py
@@ -10,19 +10,19 @@ from sklearn import linear_model, cross_validation
 # Generate training and test data using train_X, train_Y convention for LR
 # take no of tweets as train_label & rest of the features as train data or independent vars
 
-path = "../../Datasets/tweets/tweet_data/"
-#path = "F:/tweets/"
+#path = "../../Datasets/tweets/tweet_data/"
+path = "F:/tweets/"
 
 file_list = []
-file_list = os.listdir(path)
-#file_list = ["subset.txt"]
+#file_list = os.listdir(path)
+file_list = ["subset.txt"]
 
 window_size = 11
 
 for f in file_list:
 
-    print("Linear Regression on file", f)
-    print("window size", window_size)
+    print("Linear Regression on file : ", f)
+    print("window size : ", window_size)
 
     f = path + f
 
@@ -38,50 +38,55 @@ for f in file_list:
     #generate data for window & perform 10-fold cross-validation and regression
     window_mean_rmse = 0.0
 
-
+    # traverse through entire file using sliding windows [1..n], [2..n+1] ...
     for i in range(rows - 1):
         window_end = i + window_size - 1
         if window_end >= rows:
             break
         window_count += 1
         # Generate training and test data from current window
-        train_label = X[i: window_end, 0]
-        train_features = X[i: window_end, [1, 4]]
+
+        data_labels = X[i: window_end, 0]
+        data_features = X[i: window_end, [1, 4]]
+        '''
         test_label = X[window_end, 0]
         test_features = X[ window_end , [1,4]]
-
-
-        avg_error = 0.0
-        error = 0.0
+        '''
 
         regr = linear_model.LinearRegression()
         model = linear_model.LinearRegression()
-        kf = cross_validation.KFold(rows, 10, True)
+        # shuffling the window data
+        #kf = cross_validation.KFold(window_size, 10, True)
 
-        for train_index, test_index in kf:
-            X_train, X_test = X[train_index, 1:], X[test_index, 1:]
-            Y_train, Y_test = X[train_index, 0], X[test_index, 0]
+        predicted_tweet_count = cross_validation.cross_val_predict(model, data_features, data_labels, 10, 1, 0, None, 0)
+        print ("predicted_tweet_counts : ", predicted_tweet_count)
 
-            regr.fit(X_train, Y_train) # to get co-efficients
+        # used mean_absolute_error function
+        # returns avg diff between predicted and actual tweets, over 10 folds in a window
+        # doesnt randomize the input
+        scores = cross_validation.cross_val_score(model, data_features, data_labels,  cv=10, scoring='mean_absolute_error')
+        avg_scores = numpy.average(numpy.abs(scores))
+        print("avg window error : ", avg_scores)
 
+        hashtag_rmse += avg_scores
+        #print("window  RMSE", window_mean_rmse)
+        #for train_index, test_index in kf:
+            #X_train, X_test = X[train_index, 1:], X[test_index, 1:]
+            #Y_train, Y_test = X[train_index, 0], X[test_index, 0]
+
+            #regr.fit(X_train, Y_train) # to get co-efficients
         # issues - what data shud be passed to predict - test or whole ?
-        predicted = cross_validation.cross_val_predict(model, train_features, train_label, 10, 1, 0, None, 0)
-        scores = cross_validation.cross_val_score(model, train_features, train_label,  cv=10, scoring='mean_squared_error')
 
-        window_mean_rmse = numpy.mean(numpy.sqrt(-scores))
-        print 'Mean RMSE',  numpy.mean(numpy.sqrt(-scores))
-        print 'Best RMSE',  numpy.min(numpy.sqrt(-scores))
+        #print 'Best RMSE',  numpy.min(numpy.sqrt(-scores))
 
             #print("Co-efficients : ", regr.coef_)
             #print("Residual sum of squares: %.2f"% numpy.mean((regr.predict(Y_test) - X_test) ** 2))
             #predicted_tweets = regr.predict(X_test)
             #error += numpy.mean((predicted_tweets - Y_test) ** 2)
 
-        hashtag_rmse += window_mean_rmse
-        print("window_avg_error", window_avg_error)
-
+    # Avg error over all windows of a file
     avg_error = float(hashtag_rmse) / float(window_count)
-    print("hashtag avg rmse for file "+ f, avg_error)
+    print("hashtag avg absolute error for file : "+ f +" ", avg_error)
 
 
 
