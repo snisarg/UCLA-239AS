ced5f279d08a1a0d8c0989eee9fcc97965b9bd2b
Nisarg Shah <snisarg@gmail.com>
2016-03-17 00:42:22 -0700

    Script to extract specific columns

---
+++ Projects/4/export_tweets.py
@@ -0,0 +1,21 @@
+import json
+from pandas.io.json import json_normalize
+import codecs
+import pandas
+
+# result_file = codecs.open('../../Datasets/tweets/generated/Tweets_only.csv', 'a')
+tweets = []
+with codecs.open('../../Datasets/tweets/tweet_data/tweets_#gohawks.txt') as data_file:
+    for line in data_file:
+        tweet_obj = json_normalize(json.loads(line)['tweet'])
+        # data.append(pandas.Series([tweet_obj['text'][0]]))
+        tweets.append(tweet_obj['text'][0])
+        #result_file.write(unicode(str(tweet_obj['text'][0])))
+        #result_file.write('\n')
+        # print tweet_obj['text'][0]
+
+# print data.head()
+# result_file.close()
+data = pandas.Series(tweets)
+data.to_csv('../../Datasets/tweets/generated/Tweets_only.csv', encoding='utf-8')
+
